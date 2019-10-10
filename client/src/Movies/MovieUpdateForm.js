a343764217e52302055c5c9a78462d7cebf99f2b
Brandon Nichols <brandonaronnichols@gamil.com>
2019-10-10 00:57:06 -0500

    added input fields for form that obtain user input and on submit send a post request to api

--- client/src/Movies/MovieUpdateForm.js
+++ client/src/Movies/MovieUpdateForm.js
@@ -1,23 +1,69 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const formStyle = {
+    margin: '0 auto',
+    maxWidth: '200px',
+    display: 'flex',
+    flexDirection: 'column'
+}
+
 const MovieUpdateForm = props => {
-    const [movieToEdit, setMovieToEdit] = useState([]);
+    const [movieToEdit, setMovieToEdit] = useState({});
+    
     useEffect(() => {
         const movieId = props.match.params.id;
         axios
             .get(`http://localhost:5000/api/movies/${movieId}`)
             .then(res => {
-                console.log('RES FROM UPDATE FORM: ', res);
+                setMovieToEdit(res.data);
             })
             .catch(err => console.log(err.response));
     }, [props.match.params.id]);
 
+    const handleInput = e => {
+        setMovieToEdit({...movieToEdit, 
+                        [e.target.name]: e.target.value});
+    }
+
+    const handleStarsInput = (e, index) => {
+        const tempArr = movieToEdit.stars;
+        tempArr[index] = e.target.value;
+        setMovieToEdit({...movieToEdit, [e.target.name]: tempArr});
+    }
+
+    const handleUpdateSubmit = e => {
+        e.preventDefault();
+        axios
+            .put(`http://localhost:5000/api/movies/${movieToEdit.id}`, movieToEdit)
+            .then(res => props.history.push(`/movies/${movieToEdit.id}`))
+            .catch(err => console.log('Error: ', err));
+    }
+
     return(
-        <form>
+        <form style={formStyle} onSubmit={handleUpdateSubmit}>
             <input type='text' 
                    name='title' 
-                   value='fixed'/>
+                   value={movieToEdit.title}
+                   onChange={handleInput}/>
+            <input type='text' 
+                    name='director' 
+                    value={movieToEdit.director}
+                    onChange={handleInput}/>
+            <input type='text' 
+                   name='metascore' 
+                   value={movieToEdit.metascore}
+                   onChange={handleInput}/>
+            {
+                typeof movieToEdit.stars !== 'undefined' ?
+                movieToEdit.stars.map((star, index) => {
+                    return <input type='text' 
+                                name='stars' 
+                                value={star}
+                                onChange={(e) => handleStarsInput(e, index)}/> 
+                }) : null
+            }
+            <button type='submit'>Submit</button>
         </form>
     )
 }
